9f54093f75c1c52613e9b865844dacb4841a7de6
Tom Heslop <t_heslop@hotmail.co.uk>
2015-05-28 17:08:38 +0100

    improve palettes - i think

--- R/team_pal.R
+++ R/team_pal.R
@@ -1,3 +1,31 @@
+#' List of Premiership team palettes
+#' 
+#' Use \code{\link{team_pal}} to construct palettes
+#' 
+#' @export
+epl_palettes <- list(
+    "Arsenal" = c("#EF0107", "#023474", "#9C824A"),
+    "Aston Villa" = c("#7A003C", "#94BEE5", "#FFE600"),
+    "Burnley" = c("#53162F", "#8CCCE5", "#F9EC34"),
+    "Chelsea" = c("#034694", "#DBA111", "#ED1C24"),
+    "Crystal Palace" = c("#1B458F", "#C4122E", "#A7A5A6"),
+    "Everton" = c("#274488", "#FFFFFF", "#000000"),
+    "Hull" = c("#F5A12D", "#000000", "#94BEE5"),
+    "Leicester" = c("#0053A0", "#FDBE11", "#FFE600"),
+    "Liverpool" = c("#D00027", "#00A398", "#FEF667"),
+    "Man City" = c("#5DBFEB", "#FFCE65", "#FFE600"),
+    "Man Utd" = c("#DA020E", "#FFE500", "#000000"),
+    "Newcastle" = c("#231F20", "#00B6F1", "#BBBDBF"),
+    "QPR" = c("#005CAB", "#D00027", "#FFFFFF"),
+    "Southampton" = c("#ED1A3B", "#211E1F", "#FFC20E"),
+    "Spurs" = c("#001C58", "#FFFFFF", "#000000"),
+    "Stoke" = c("#E03A3E", "#1B449C", "#94BEE5"),
+    "Sunderland" = c("#EB172B", "#A68A26", "#211E1E"),
+    "Swansea" = c("#000000", "#DA020E", "#FFFFFF"),
+    "West Brom" = c("#091453", "#FFFFFF", "#EB172B"),
+    "West Ham" = c("#60223B", "#F7C240", "#5299C6")
+)
+
 #' Premiership Team colors palette
 #' 
 #' Palette of primary team colors where possible - the secondary color is used
@@ -7,48 +35,39 @@
 #' of the datasets in this package.  It can simply be called within a ggplot 
 #' plot, see \emph{examples}
 #' 
-#' @param \strong{teams} specify teams (not always necessary)
+#' @param teams specify teams (not always necessary)
+#' @param simple return a single colour per team
+#' @param n number of colours to return in a palette (per team) (maximum of 3)
+#' 
 #' @export
 #' @examples
+#' \dontrun{
 #' team_pal(teams = c("Arsenal", "Aston Villa"))
-#'    Arsenal Aston Villa 
-#'  "#F12933"   "#A32249" 
 #'
 #' # using within ggplot
 #' data(players)
 #' ggplot(players, aes(x=value, y=pts, color=team)) +
 #'      geom_point() +
 #'      scale_color_manual(values = team_pal()) 
-team_pal <- function(teams = NULL) {
-    
-    pal <- c('Arsenal'="#F12933", 
-             'Aston Villa'="#A32249", 
-             'Burnley'="#A32249", 
-             'Chelsea'="#4358AB", 
-             'Crystal Palace'="#F12933", 
-             'Everton'="#0B4A99",
-             'Hull'="#FD9A34", 
-             'Leicester'="#0B4A99", 
-             'Liverpool'="#F12933", 
-             'Man City'="#98CDEC", 
-             'Man Utd'="#F12933", 
-             'Newcastle'="#000000",
-             'QPR'="#0B4A99", 
-             'Southampton'="#F12933", 
-             'Spurs'="#182341", 
-             'Stoke'="#F12933", 
-             'Sunderland'="#F12933", 
-             'Swansea'="#000000",
-             'West Brom'="#182341",
-             'West Ham'="#A32249")
-    
+#' }
+team_pal <- function(teams = NULL, simple = TRUE, n = 1) {
+
     if(is.null(teams)) {
+        # if no teams are entered return all 20
+        pal <- lapply(epl_palettes, function(x) x[1])
+        pal <- unlist(pal)
+        return(pal)
+    }
+    
+    teampals <- lapply(teams, function(x) epl_palettes[[x]])
+    names(teampals) <- teams
+    
+    if(simple && n == 1) {
+        pal <- unlist(lapply(teampals, function(x) x[1]))
         return(pal)
     } else {
-        new_pal <- vector()
-        for(team in teams) {
-            new_pal[team] <- pal[team]
-        }
-        return(new_pal)
+        if(n > 3) stop("Number of requested colours greater than number available, choose 3 or fewer.")
+        pal <- lapply(teampals, function(x) x[1:n])
+        return(pal)
     }
 }
