e9752cf29ae5182355842baad5e4793fae4fe472
Tom Heslop <t_heslop@hotmail.co.uk>
2015-07-15 14:19:15 +0100

    update for 2015-16 season data

--- R/fantasysocceR.R
+++ R/fantasysocceR.R
@@ -10,8 +10,13 @@
 #' 
 #' Datasets include:
 #' \itemize{
-#'      \item \strong{players} Player summary data, see \link{players}
-#'      \item \strong{pastseasons} Past season data, see \link{pastseasons}
+#'      \item \strong{players} Player summary data for 2015/16 season, see \link{players}
+#'      \item \strong{pastseasons} Past season data for the 2015/16 season, see
+#'      \link{pastseasons}
+#'      \item \strong{players201415} Player summary data for 2014/15 season, 
+#'      see \link{players201415}
+#'      \item \strong{pastseasons201415} Past season data for 2014/15 season, 
+#'      see \link{pastseasons201415}
 #'      \item \strong{season201415} Performance data for the 2014/15 Season, see \link{season201415}
 #' }
 #' @name fantasysocceR
@@ -22,12 +27,13 @@ NULL
 #' 
 #' @description Summary data about the current players in the game, some 
 #' variables in this dataset will almost immediately be out of date (such as 
-#' \emph{pct}).  See \link{collect_ff} for getting up-to-date data.
+#' \emph{pct}), data collected Jul 15, 2015.
+#' See \link{collect_ff} for getting up-to-date data.
 #' 
 #' @docType data
 #' @keywords datasets
 #' @name players
-#' @format 554 rows, 7 columns
+#' @format 502 rows, 7 columns
 #' @details Variables
 #' \itemize{
 #'      \item \strong{pos} player pos
@@ -45,7 +51,7 @@ NULL
 #' @description Data about the current players in the game and their performance
 #' in past seasons.  Unlike \link{players} dataset, the
 #' majority of the variables in this dataset should be stable, but it does
-#' include some of the data found in \link{players}.  
+#' include some of the data found in \link{players}, data collected Jul 15, 2015.  
 #' 
 #' @details If a player was included in more than one previous version of the 
 #' game, then that player will have multiple rows.  If a player is new to the 
@@ -55,6 +61,73 @@ NULL
 #' @docType data
 #' @keywords datasets
 #' @name pastseasons
+#' @format 1869 rows, 24 columns
+#' @details Variables
+#' \itemize{
+#'      \item \strong{pos} player pos
+#'      \item \strong{team} player team
+#'      \item \strong{pts} total points scored last season
+#'      \item \strong{id} player id
+#'      \item \strong{name} player name
+#'      \item \strong{value} player value
+#'      \item \strong{pct} percentage of fantasy teams player is selected by
+#'      \item \strong{season} past season
+#'      \item \strong{mins} number of minutes played
+#'      \item \strong{goals} goals scored
+#'      \item \strong{assists} assists
+#'      \item \strong{cs} clean sheets
+#'      \item \strong{og} own goals
+#'      \item \strong{ga} goals against
+#'      \item \strong{pens_svd} penalties saved
+#'      \item \strong{pens_msd} penalties missed
+#'      \item \strong{yel} yellow cards
+#'      \item \strong{red} red cards
+#'      \item \strong{saves} number of saves (keepers)
+#'      \item \strong{bonus} bonus points
+#'      \item \strong{ea_ppi} ea sports player performace index
+#'      \item \strong{bps} bonus points system
+#'      \item \strong{fin_val} player value at close of season
+#'      \item \strong{ssn_pts} total points for that season
+#' }
+NULL
+
+#' Players 2014/15 dataset
+#' 
+#' @description Summary data about the current players in the game, some 
+#' variables in this dataset will almost immediately be out of date (such as 
+#' \emph{pct}).  See \link{collect_ff} for getting up-to-date data.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name players201415
+#' @format 554 rows, 7 columns
+#' @details Variables
+#' \itemize{
+#'      \item \strong{pos} player pos
+#'      \item \strong{team} player team
+#'      \item \strong{pts} total points scored last season
+#'      \item \strong{id} player id
+#'      \item \strong{name} player name
+#'      \item \strong{value} player value
+#'      \item \strong{pct} percentage of fantasy teams player is selected by
+#' }
+NULL
+
+#' Past seasons 2014/15 dataset
+#' 
+#' @description Data about the current players in the game and their performance
+#' in past seasons.  Unlike \link{players} dataset, the
+#' majority of the variables in this dataset should be stable, but it does
+#' include some of the data found in \link{players}.  
+#' 
+#' @details If a player was included in more than one previous version of the 
+#' game, then that player will have multiple rows.  If a player is new to the 
+#' game, they will have summary data found in \link{players} but past seasons
+#' will be filled with NAs.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name pastseasons201415
 #' @format 1920 rows, 24 columns
 #' @details Variables
 #' \itemize{
